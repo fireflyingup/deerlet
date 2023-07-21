83277106c2a023ec045c78cccfce805b70cf781c
fire flying <caogj@tcsec.com.cn>
2023-07-21 17:26:11 +0800

    add agent test

--- boot/src/main/java/com/fireflyingup/deerlet/boot/Main.java
+++ boot/src/main/java/com/fireflyingup/deerlet/boot/Main.java
@@ -4,8 +4,11 @@ package com.fireflyingup.deerlet.boot;
 import com.fireflyingup.deerlet.boot.utils.JvmEnvUtils;
 import com.fireflyingup.deerlet.common.CommandUtils;
 import com.fireflyingup.deerlet.common.PrintLog;
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
 import org.apache.commons.lang3.ObjectUtils;
 
+import java.io.IOException;
 import java.util.*;
 
 /**
@@ -21,7 +24,14 @@ import java.util.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        String pid = findPid();
+        VirtualMachine virtualMachine = null;
+        virtualMachine = VirtualMachine.attach(pid);
+        virtualMachine.loadAgent("/Users/guoying/big/myProject/java/deerlet/agent/target/arthas-agent-jar-with-dependencies.jar");
+    }
+
+    private static String findPid( ) {
         String jps = JvmEnvUtils.getJps();
         List<String> run = CommandUtils.run(jps, "-l");
         if (ObjectUtils.isEmpty(run)) {
@@ -32,27 +42,38 @@ public class Main {
         PrintLog.info("choose a process to attack, for example: choose 1 then enter");
         Map<Integer, String> map = new HashMap<>();
         for (String s : run) {
+            String[] split = s.split("\\s+");
+            if (split.length < 1) {
+                continue;
+            }
+            if (split.length > 1 && isJpsProcess(split[1])) {
+                continue;
+            }
             if (index == 1) {
                 System.out.println("* " + index + " " + s);
             } else {
                 System.out.println("  " + index + " " + s);
             }
-            map.put(index++, s);
+            map.put(index++, split[0].trim());
         }
 
         String line = new Scanner(System.in).nextLine();
+        String pidInfo;
         if (line.trim().isEmpty()) {
-            System.out.println(map.get(1));
-            return;
-        }
-        int i = new Scanner(line).nextInt();
-        String s = map.get(i);
-        if (ObjectUtils.isEmpty(s)) {
-            PrintLog.error("no such progress find ");
-            System.exit(1);
+            pidInfo = map.get(1);
+        } else {
+            int i = new Scanner(line).nextInt();
+            pidInfo = map.get(i);
+            if (ObjectUtils.isEmpty(pidInfo)) {
+                PrintLog.error("no such progress find ");
+                System.exit(1);
+            }
         }
 
-        System.out.println(s);
+        return pidInfo;
     }
 
+    private static boolean isJpsProcess(String mainClassName) {
+        return "sun.tools.jps.Jps".equals(mainClassName) || "jdk.jcmd/sun.tools.jps.Jps".equals(mainClassName);
+    }
 }
