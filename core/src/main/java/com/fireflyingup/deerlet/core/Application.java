c184e5f02e39186d9519d3a11f70be04c8cb303c
fire flying <caogj@tcsec.com.cn>
2023-08-23 15:35:52 +0800

    添加git ignore忽略

--- core/src/main/java/com/fireflyingup/deerlet/core/Application.java
+++ core/src/main/java/com/fireflyingup/deerlet/core/Application.java
@@ -1,19 +1,15 @@
 package com.fireflyingup.deerlet.core;
 
-import com.fireflyingup.deerlet.common.PrintLog;
 import com.fireflyingup.deerlet.core.transformer.CustomClassFileTransformer;
-import com.sun.corba.se.impl.orbutil.ObjectUtility;
 import org.apache.commons.lang3.ObjectUtils;
 
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
 
 /**
-*
-* @author: Fire Flying
-*
-* @create: 2023-07-25 15:46
-**/
+ * @author: Fire Flying
+ * @create: 2023-07-25 15:46
+ **/
 
 public class Application {
 
@@ -32,7 +28,7 @@ public class Application {
         try {
             if (ObjectUtils.isNotEmpty(allLoadedClasses)) {
                 for (Class<?> clazz : allLoadedClasses) {
-                    if(inst.isModifiableClass(clazz)) {
+                    if (inst.isModifiableClass(clazz)) {
                         inst.retransformClasses(clazz);
                     }
                 }
