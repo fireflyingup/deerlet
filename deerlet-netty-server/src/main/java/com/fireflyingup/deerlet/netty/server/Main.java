88023964a50b4b972f74863af7f5ae5564038f59
fireflyingup <1301501708@qq.com>
2023-09-18 23:20:34 +0800

    提交代码

--- deerlet-netty-server/src/main/java/com/fireflyingup/deerlet/netty/server/Main.java
+++ deerlet-netty-server/src/main/java/com/fireflyingup/deerlet/netty/server/Main.java
@@ -10,7 +10,7 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        DeerletNettyServer deerletNettyServer = new DeerletNettyServer();
+        DeerletNettyServer deerletNettyServer = new DeerletNettyServer("127.0.0.1", 6666);
         deerletNettyServer.start();
     }
 
